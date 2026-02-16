c5f5ed8b6ce9c760f004910ac26e39ef68f0dc30
jesus282 <jesus26@gmail.com>
2026-02-16 16:24:14 -0600

    canvas personalizado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-function draw() {
-  const canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
+// Se obtiene una referencia al elemento <canvas> del documento HTML
+var canvas = document.getElementById("lienzo");
 
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
-  }
-}
+// Se obtiene el contexto de renderizado en 2D del canvas
+var ctx = canvas.getContext("2d");
+
+// Ajustamos el tamaño del canvas al tamaño completo de la ventana
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Calculamos el 50% del ancho y alto de la pantalla
+var rectWidth = window.innerWidth * 0.5;
+var rectHeight = window.innerHeight * 0.5;
+
+// Color de relleno
+ctx.fillStyle = "rgb(79, 241, 46)";
+
+// Dibujamos el rectángulo ocupando 50% del ancho y 50% del alto
+ctx.fillRect(10, 10, rectWidth, rectHeight);
